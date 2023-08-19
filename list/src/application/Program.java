c97030f9327acec654218a26c818842dc82f45a1
kali <kali@dellkali.jrdellkali.net.br>
2023-08-19 11:57:53 -0300

    list remove

--- list/src/application/Program.java
+++ list/src/application/Program.java
@@ -19,11 +19,24 @@ public class Program {
 		list.add("Elias");
 		list.add(0, "JAIR");
 
-		
-		//tamanho da lista
-		System.out.println(list.size());
-		
 		int i = 0;
+		// percorre a lista e imprime os elementos na posicao X
+		for (String x : list) {
+			System.out.println((i++) + " ->" + x);
+		}
+
+		System.out.println("-----------------------------");
+
+		// remove String nome
+		list.remove("Gertude");
+
+		// remove by position
+		list.remove(5);
+
+		// tamanho da lista
+		System.out.println(list.size());
+
+		i = 0;
 		// percorre a lista e imprime os elementos na posicao X
 		for (String x : list) {
 			System.out.println((i++) + " ->" + x);
